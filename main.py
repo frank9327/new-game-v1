40052bf2ea35f9bf68bbfa56e71fa6f07614a769
John Doe <johndoe@example.com>
2024-02-13 10:30:22 -0600

    hangman v1

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from sys import exit
 import random
 import sys
 pygame.init()
-screen_width = 800
-screen_height = 600
+font = pygame.font.Font("proffesional Edition.ttf", 30)
+screen_width = 500
+screen_height = 750
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Intro to Pygame: Platform Game")
 clock = pygame.time.Clock()
+
 
 
 class Square(pygame.sprite.Sprite):
@@ -20,40 +22,39 @@ class Square(pygame.sprite.Sprite):
         self.image.fill(color)
         self.rect = self.image.get_rect(center = (x,y))
         self.radius=20
-    
+   
     def move(self, deltax, deltay):
         if self.rect.left < 0:
             deltax *= -3
             self.rect.centerx=50
-        if self.rect.right>800:
+        if self.rect.right>500:
             deltax *= -3
-            self.rect.centerx=745
+            self.rect.centerx=445
         self.rect.centerx += deltax
         self.rect.centery += deltay
+
 
 class Food(pygame.sprite.Sprite):
     def __init__(self,color):
         super(Food,self).__init__()
         self.radius=5
-        self.speed=10
-        self.x = random.randint(0,650)
-        self.y = random.randint(0,500)
+        self.speed=random.randint(5,20)
+        self.x = random.randint(0,500)
+        self.y = random.randint(0,700)
         self.x_heading = (1/self.radius) * self.speed
         self.y_heading = (1/self.radius) * self.speed
         self.image=pygame.Surface((self.radius*2,self.radius*2),pygame.SRCALPHA, 32)
         self.image=self.image.convert_alpha()
         pygame.draw.circle(self.image, color, (self.radius,self.radius),self.radius)
-        self.rect=self.image.get_rect(center=(random.randint(10,790),random.randint(5,15)))
+        self.rect=self.image.get_rect(center=(random.randint(10,450),random.randint(5,80)))
+
 
     def move(self):
-        if self.rect.centerx <= -0 or self.rect.centerx >= 0:
-            self.x_heading *= -1
-        if self.rect.centery <= 0 or self.rect.centery >= 600:
+        if self.rect.centery <= 0 or self.rect.centery >= 750:
             self.y_heading *= -1
-        self.rect.x += self.x_heading
         self.rect.y += self.y_heading
 
-sq1 = Square(400,500,"pink")
+sq1 = Square(400,600,"pink")
 squares = pygame.sprite.Group()
 squares.add(sq1)
 food=pygame.sprite.Group()
@@ -61,12 +62,14 @@ edible=pygame.sprite.Group()
 edible.add(food)
 edible.add(squares)
 
-alive=5
-for num in range(0,alive):
-    mealColor=(random.randint(0,0),random.randint(0,0),random.randint(50,255))
+spawn=random.randint(3,6)
+alive=spawn
+for num in range(spawn):
+    # mealColor=(random.randint(0,0),random.randint(0,0),random.randint(50,255))
+    mealColor="White"
     food.add(Food(mealColor))
-
-
+points=0
+misses=0
 running = True
 while running:
     for event in pygame.event.get():
@@ -74,30 +77,49 @@ while running:
             running = False
     screen.fill((0,0,0))
     keys = pygame.key.get_pressed()
-    
+   
     if keys[pygame.K_LEFT]:
         sq1.move(-4,0)
     if keys[pygame.K_RIGHT]:
         sq1.move(4,0)
 
-    points=0
-    win=False
     for fruit in food:
         fruit.move()
         if sq1.rect.colliderect(fruit.rect):
             fruit.kill()
-            points=1
-            alive=alive-1
-            if points>=alive:
-                win=True
-    if win==True:
-        points=0
-        for num in range(0,alive):
-            mealColor=(random.randint(0,0),random.randint(55,255),random.randint(0,0))
-            food.add(Food(mealColor))
+            alive-=1
+            points+=1
+        if fruit.rect.centery>sq1.rect.centery+30:
+            fruit.kill()
+            misses+=1
+            alive-=1
+        if alive<1:
+            alive=spawn
+            for num in range(spawn):
+                # mealColor=(random.randint(0,0),random.randint(0,0),random.randint(50,255))
+                food.add(Food(mealColor))
+        if misses>=10:
+            text=str("You lose")
+            text = font.render(text, True, (200,0,0))
+            screen.blit(text, (200, 350))
+            alive=9
+        if points>=1:
+            text=str("You Win")
+            text = font.render(text, True, (250,250,0))
+            screen.blit(text, (200, 350))
+            alive=9
     squares.draw(screen)
     food.draw(screen)
+    text=str(f"{str(points)} points")
+    text = font.render(text, True, (255,255,255))
+    screen.blit(text, (10, 10))
+    text=str(f"{str(misses)} misses")
+    text = font.render(text, True, (255,255,255))
+    screen.blit(text, (350, 10))
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
 sys.exit()
+
+
+
