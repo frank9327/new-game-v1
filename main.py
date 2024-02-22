4447c4c962846152c1ae148bdb54c8eab8d1f24b
John Doe <johndoe@example.com>
2024-02-22 10:02:59 -0600

    final

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ clock = pygame.time.Clock()
 class Square(pygame.sprite.Sprite):
     def __init__(self,x,y,color):
         super(Square, self).__init__()
-        self.image = pygame.Surface((100,100), pygame.SRCALPHA, 32)
-        self.image.convert_alpha()
-        self.image.fill(color)
+        self.image = pygame.image.load("basketv2.png")
         self.rect = self.image.get_rect(center = (x,y))
         self.radius=20
    
@@ -43,11 +41,34 @@ class Food(pygame.sprite.Sprite):
         self.y = random.randint(0,700)
         self.x_heading = (1/self.radius) * self.speed
         self.y_heading = (1/self.radius) * self.speed
-        self.image=pygame.Surface((self.radius*2,self.radius*2),pygame.SRCALPHA, 32)
-        self.image=self.image.convert_alpha()
+        self.image=pygame.image.load("chickwingsdown.png")
         pygame.draw.circle(self.image, color, (self.radius,self.radius),self.radius)
         self.rect=self.image.get_rect(center=(random.randint(10,450),random.randint(5,80)))
 
+    def move(self):
+        count=0
+        if self.rect.centery <= 0 or self.rect.centery >= 750:
+            self.y_heading *= -1
+            count+=1
+        self.rect.y += self.y_heading
+        if self.rect.y%5==0:
+            self.image=pygame.image.load("chickwingsup.png")
+        else:
+            self.image=pygame.image.load("chickwingsdown.png")
+
+
+class fastFood(pygame.sprite.Sprite):
+    def __init__(self,color):
+        super(fastFood,self).__init__()
+        self.radius=5
+        self.speed=random.randint(5,20)
+        self.x = random.randint(0,500)
+        self.y = random.randint(0,700)
+        self.x_heading = (1/self.radius) * self.speed
+        self.y_heading = (1/self.radius) * self.speed
+        self.image=pygame.image.load("bluebird.png")
+        pygame.draw.circle(self.image, color, (self.radius,self.radius),self.radius)
+        self.rect=self.image.get_rect(center=(random.randint(10,450),random.randint(5,80)))
 
     def move(self):
         if self.rect.centery <= 0 or self.rect.centery >= 750:
@@ -58,16 +79,22 @@ sq1 = Square(400,600,"pink")
 squares = pygame.sprite.Group()
 squares.add(sq1)
 food=pygame.sprite.Group()
+fastfood=pygame.sprite.Group()
 edible=pygame.sprite.Group()
+birds=pygame.sprite.Group()
+birds.add(food)
+birds.add(fastfood)
 edible.add(food)
+edible.add(fastfood)
 edible.add(squares)
 
 spawn=random.randint(3,6)
 alive=spawn
 for num in range(spawn):
     # mealColor=(random.randint(0,0),random.randint(0,0),random.randint(50,255))
-    mealColor="White"
+    mealColor="Black"
     food.add(Food(mealColor))
+
 points=0
 misses=0
 running = True
@@ -83,8 +110,13 @@ while running:
     if keys[pygame.K_RIGHT]:
         sq1.move(4,0)
 
+    if points>10:
+        for num in range(spawn):
+            fastfood.add(fastFood(mealColor))
     for fruit in food:
         fruit.move()
+    # for fruit in fastfood:
+    #     fruit.move()
         if sq1.rect.colliderect(fruit.rect):
             fruit.kill()
             alive-=1
@@ -96,18 +128,19 @@ while running:
         if alive<1:
             alive=spawn
             for num in range(spawn):
-                # mealColor=(random.randint(0,0),random.randint(0,0),random.randint(50,255))
                 food.add(Food(mealColor))
         if misses>=10:
             text=str("You lose")
             text = font.render(text, True, (200,0,0))
             screen.blit(text, (200, 350))
             alive=9
-        if points>=1:
+            break
+        if points>=30:
             text=str("You Win")
             text = font.render(text, True, (250,250,0))
             screen.blit(text, (200, 350))
             alive=9
+            break
     squares.draw(screen)
     food.draw(screen)
     text=str(f"{str(points)} points")
@@ -120,6 +153,3 @@ while running:
     clock.tick(60)
 pygame.quit()
 sys.exit()
-
-
-
